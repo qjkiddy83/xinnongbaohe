8638590e9498bd1c019b3873288db335ef777454
qjkiddy83 <qjkiddy-83@163.com>
2017-09-03 20:37:29 +0800

    xinnongbaohe

--- background/src/router/global.js
+++ background/src/router/global.js
@@ -1,2 +1,4 @@
 import '../css/reset.css';
 import '../css/base.css';
+let $ = require('jquery');
+$('.nav,.right-cont').css('min-height',$(window).height())
--- background/src/router/index.js
+++ background/src/router/index.js
@@ -1,3 +1,1 @@
 import './global';
-import '../css/index.css';
-import '../js/index';//同 require('../js/index')
